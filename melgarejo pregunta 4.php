834b0ad93b2e79ad7c13733530fdd4d12b8f7d54
hernan30mel <97269571+hernan30mel@users.noreply.github.com>
2022-01-26 13:46:14 -0500

    Add files via upload

---
+++ melgarejo pregunta 4.php
@@ -0,0 +1,18 @@
+<?php
+{
+    "name": "melgarejo/practica",
+    "description": "practica de php + mysql",
+    "require-dev": {
+        "php": "^8.0.13"
+    },
+    "require": {
+        "illuminate/database": "^8.80",
+        "illuminate/events": "^8.80"
+    },
+    "autoload": {
+        "psr-4": {
+            "Monolog\\": "[src/]",
+            "Vendor\\Namespace\\": ""
+        }
+    }
+}
